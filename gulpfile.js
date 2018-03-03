80d7562b00b39ad0d6aa2903eb504ff319609928
m <xiaoham3@uci.edu>
2018-03-02 18:53:43 -0800

    debug

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import');
-
+mixins = require('postcss-mixins');
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
 });
@@ -16,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
